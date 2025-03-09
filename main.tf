9f4b26b60fd2e0c1dd2b423cbb859ad2c9e13121
Rajani0206 <rajanisc15@gmail.com>
2025-03-09 17:48:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,22 @@ module "EC2_Module" {
   source  = "github.com/Rajani0206/Terraform_Modules"
   vpc_cidr_block = var.vpc_cidr_block
   instance_type = var.instance_type
-  environment = "${terraform.workspace}"
   ami_id= "ami-00bb6a80f01f03502"
+  user_data = <<-EOF
+              #!/bin/bash
+              # Update the system
+              sudo apt update -y
+
+              # Install dependencies Ansible
+              sudo apt install ansible -y
+
+              # Install Terraform
+              cd /tmp
+              curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo tee /etc/apt/trusted.gpg.d/hashicorp.asc
+              sudo apt-get update && sudo apt-get install terraform -y
+  tags = {
+    Name = "TerraformAnsibleInstance"
+  }
+
   
 }
